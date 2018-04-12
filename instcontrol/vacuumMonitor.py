076257939ddbd8583c70c213be624d6ba22d383d
chw3k5 <chw3k5@gmail.com>
2018-04-11 20:10:37 -0700

    I have improved the vacuum gauge code with more error checking. to get a good reading. I have created reading file for the Lake Shore 224 and 336. Also a data logging code has been added to record any monitored information.

--- instcontrol/vacuumMonitor.py
+++ instcontrol/vacuumMonitor.py
@@ -3,13 +3,7 @@ This is for the the Bayard-Alpert Pirani Gauge BPG400 family of vacuum gauges fr
 It is anticipated that you you are using a RS232 (9 pin serial cable) to read this gauge.
 For more information needed to wire the the RS232 cable, consult the operating manual for this device.
 """
-
-from matplotlib import pyplot as plt
-import numpy as np
-import os, sys,time, serial
-
-from instcontrol.SerialTests import findSerialPort, testSerialPortHasDeviceWindows
-
+import time, serial
 
 """
 This tries to open a serial port where the vacuum gauge is located. 
@@ -22,15 +16,17 @@ bytesize = 8
 stopbits = 1
 timeout = 2
 
-if testSerialPortHasDeviceWindows(portName, baudrate, bytesize, stopbits, timeout):
-    vacGauge = serial.Serial(port=portName, baudrate=baudrate, bytesize=bytesize, stopbits=stopbits, timeout=timeout)
-
 def getPressure(verbose=False):
-    for sleepTime in [0.0, 0.1, 0.2, 1]:
+    vacGauge = serial.Serial(port=portName, baudrate=baudrate, bytesize=bytesize, stopbits=stopbits, timeout=timeout)
+    for sleepTime in [0.0, 0.1, 0.2, 1.0]:
         byteOrder = "little"
-        lenOfDataString = int.from_bytes(vacGauge.read(), byteorder=byteOrder)
+        lenOfDataString = -1
+        while lenOfDataString != 7:
+            lenOfDataString = int.from_bytes(vacGauge.read(), byteorder=byteOrder)
         time.sleep(sleepTime)
-        pageNumber = int.from_bytes(vacGauge.read(), byteorder=byteOrder)
+        pageNumber = -1
+        while pageNumber != 5:
+            pageNumber = int.from_bytes(vacGauge.read(), byteorder=byteOrder)
         time.sleep(sleepTime)
         status = int.from_bytes(vacGauge.read(), byteorder=byteOrder)
         time.sleep(sleepTime)
@@ -40,7 +36,9 @@ def getPressure(verbose=False):
         time.sleep(sleepTime)
         lowByte = int.from_bytes(vacGauge.read(), byteorder=byteOrder)
         time.sleep(sleepTime)
-        solfwareVersion = int.from_bytes(vacGauge.read(), byteorder=byteOrder)
+        solfwareVersion = -1
+        while solfwareVersion != 44:
+            solfwareVersion = int.from_bytes(vacGauge.read(), byteorder=byteOrder)
         time.sleep(sleepTime)
         sensorType = int.from_bytes(vacGauge.read(), byteorder=byteOrder)
         time.sleep(sleepTime)
@@ -54,25 +52,37 @@ def getPressure(verbose=False):
             print(error, "is the error byte")
             print(highByte, "is the high measurement byte need to calculate the pressure")
             print(lowByte, "is the low measurement byte need to calculate the pressure")
-            print(solfwareVersion, "is the software version byte,", float(solfwareVersion) / 20.0, "is the software version")
+            print(solfwareVersion, "is the software version byte,", float(solfwareVersion) / 20.0,
+                  "is the software version")
             print(sensorType, "is the sensor type byte, should be 10 for BPG400 vacuum gauge")
             print(checkSum, "is the check sum byte, used for synchronization ")
             print(measSum, "is the measured sum and should be equal to the check sum")
             print()
         if measSum == checkSum:
             break
+    vacGauge.close()
+    if measSum == checkSum:
+        pressureTorr = 10.0 ** (((float(highByte) * 256.0 + float(lowByte)) / 4000.0) - 12.625)
+    else:
+        print("Something is wrong with the vacuum gauge measSum is not equal to checkSum")
+        print(lenOfDataString, "is the length of data string byte, should be 7")
+        print(pageNumber, "is the page number byte, should be 5")
+        print(status, "is the status byte")
+        print(error, "is the error byte")
+        print(highByte, "is the high measurement byte need to calculate the pressure")
+        print(lowByte, "is the low measurement byte need to calculate the pressure")
+        print(solfwareVersion, "is the software version byte,", float(solfwareVersion) / 20.0,
+              "is the software version")
+        print(sensorType, "is the sensor type byte, should be 10 for BPG400 vacuum gauge")
+        print(checkSum, "is the check sum byte, used for synchronization ")
+        print(measSum, "is the measured sum and should be equal to the check sum")
+        print("Setting pressure to -1.0 to flag as an error for later!\n")
+        pressureTorr = -1.0
 
-    pressureTorr =  10.0**(((float(highByte) * 256.0 + float(lowByte)) / 4000.0) - 12.625)
     return pressureTorr
 
-def closeVacGauge():
-    vacGauge.close()
-    return
-
-def makeTestPlot():
-    return
-
+def getFormattedPressure(verbose=False):
+    return str('%1.3e' % getPressure(verbose=verbose))
 
 if __name__ == "__main__":
-    print(str('%1.3e' % getPressure(verbose=True)), "torr")
-    closeVacGauge()
+    print(getFormattedPressure(True), "torr")
