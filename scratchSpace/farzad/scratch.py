52bfb0e4a4434dd692f9bc7170cf42c1f15bd390
ffaramar <ffarama1@asu.edu>
2018-08-29 09:53:23 -0700

    initial commit

---
+++ scratchSpace/farzad/scratch.py
@@ -0,0 +1,21 @@
+print("This is the scratch program")
+
+
+def tester(verbose=False):
+    if verbose:
+        print("Verbose is on")
+    print("Verbose is", verbose, "\n")
+    return True
+
+
+if __name__ == "__main__":
+    print("scratch is the the main file")
+    tester(verbose=True)
+    tester()
+else:
+    print("scratch.py is being called from another program")
+
+
+
+
+
