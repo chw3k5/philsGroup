7bbdd9d5ca14c9596642de9bd04120fc0595dbb6
Caleb Wheeler <chw3k5@gmail.com>
2018-09-12 19:46:49 -0500

    Continuing initial work on the simple thermal model.

--- analysis/thermal/simpleModel.py
+++ analysis/thermal/simpleModel.py
@@ -6,11 +6,8 @@ thermal_conductivity_of_copper = 401.0 # Watts per m Kelvin
 
 def heatTransfer(area, length, T1, T2, thermal_conductivity=thermal_conductivity_of_copper):
     # m Kg s units
-    # Q = k * (A/L) * (T1 - T2)
-    heating_W = None
-
-
-
+    # Q = k * (A/L) * (T2 - T1)
+    heating_W = thermal_conductivity * (area / length) * (T2 - T1)
     return heating_W
 
 
@@ -20,5 +17,8 @@ if __name__ == "__main__":
     thermal_conductivity = thermal_conductivity_of_copper
     thickness = float(1) # in millimeters
     width = float(30) # in milimeters
-    area =( thickness/1000.) * (width/1000.0) # m^2
-    heatTransfer(area, length, T1, T2)
+    area =( thickness / 1000.) * (width / 1000.0) # m^2
+    length = float(100) / 1000.0
+    T1 = float(40) # in Kelvin
+    T2 = float(41) # in Kelvin
+    print(heatTransfer(area, length, T1, T2))
