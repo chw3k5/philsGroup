5aeede5b1341911d25577e52d0ea73d322e649c7
ffaramar <ffarama1@asu.edu>
2018-09-13 14:33:16 -0700

    initial commit

--- analysis/thermal/simpleModel.py
+++ analysis/thermal/simpleModel.py
@@ -18,14 +18,14 @@ if __name__ == "__main__":
     thermal_conductivity = thermal_conductivity_of_copper
     thickness = float(1) # in millimeters
     width = float(30) # in millimeters
-    area =( thickness / 1000.) * (width / 1000.0) # m^2
+    area =(thickness / 1000.) * (width / 1000.0) # m^2
     length = np.arange(0.001, 0.3, 0.05)
     T1 = float(4) # in Kelvin
-    T2 = float(41) # in Kelvin
-    heating_W = heatTransfer(area, length, T1, T2)
+    T2 = float(14) # in Kelvin
+    heating_W = float(1)
    # print(heating_W, "is the heating in Watts")
 
-plt.plot(length,heating_W)
+plt.plot(area,length)
 plt.yscale('log')
 plt.ylabel('Heat Transfer (W)')
 plt.xlabel('Length(m)')
