3ac079015126fe4a989502bd16624efdf713798f
ffarama1 <ffarama1@asu.edu>
2018-10-06 13:33:19 -0700

    m

--- solidworks/simonsASUTestCryostat/supports.py
+++ solidworks/simonsASUTestCryostat/supports.py
@@ -1,4 +1,17 @@
 import os, sys
 
-parentDirectory = ""
+parentDirectory = "C:\\Users\\ffarama1\\Documents"
 
+fileName = "SupportsEquations.txt"
+
+fullFileName = os.path.join(parentDirectory, fileName)
+
+textFile = open(fullFileName, "w")
+textFile.write("\"fiberglass_height\"=160mm\n")
+textFile.write("\"fiberglass_width\"=150mm\n")
+textFile.write("\"D1@Sketch1\"=\"fiberglass_width\"\n")
+textFile.write("\"D2@Sketch1\"=\"fiberglass_height\"\n")
+
+
+textFile.close()
+print("wrote file:", fullFileName)
