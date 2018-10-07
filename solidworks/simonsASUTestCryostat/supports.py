99fbb7183f8039ce3547b42301d5e76bfbd16677
ffarama1 <ffarama1@asu.edu>
2018-10-07 15:49:01 -0700

    m

--- solidworks/simonsASUTestCryostat/supports.py
+++ solidworks/simonsASUTestCryostat/supports.py
@@ -7,10 +7,50 @@ fileName = "SupportsEquations.txt"
 fullFileName = os.path.join(parentDirectory, fileName)
 
 textFile = open(fullFileName, "w")
-textFile.write("\"fiberglass_height\"=200mm\n")
-textFile.write("\"fiberglass_width\"=100mm\n")
+textFile.write("\"fiberglass_height\"=160mm\n")
+textFile.write("\"fiberglass_width\"=160mm\n")
+textFile.write("\"slot_length\"=\"fiberglass_width\"\n")
 textFile.write("\"D1@Sketch1\"=\"fiberglass_width\"\n")
 textFile.write("\"D2@Sketch1\"=\"fiberglass_height\"\n")
+textFile.write("\"D3@Sketch1\"=\"slot_length\"\n")
+
+
+textFile.close()
+print("wrote file:", fullFileName)
+
+
+import os, sys
+
+parentDirectory = "C:\\Users\\ffarama1\\Documents"
+
+fileName = "SupportsEquations2.txt"
+
+fullFileName = os.path.join(parentDirectory, fileName)
+
+textFile = open(fullFileName, "w")
+textFile.write("\"fiberglass_height\"=160mm\n")
+textFile.write("\"fiberglass_width\"=160mm\n")
+textFile.write("\"slot_length\"=\"fiberglass_width\"\n")
+textFile.write("\"D3@Sketch1\"=\"slot_length\"\n")
+
+
+textFile.close()
+print("wrote file:", fullFileName)
+
+
+import os, sys
+
+parentDirectory = "C:\\Users\\ffarama1\\Documents"
+
+fileName = "SupportsEquations3.txt"
+
+fullFileName = os.path.join(parentDirectory, fileName)
+
+textFile = open(fullFileName, "w")
+textFile.write("\"fiberglass_height\"=160mm\n")
+textFile.write("\"fiberglass_width\"=160mm\n")
+textFile.write("\"slot_length\"=\"fiberglass_width\"\n")
+textFile.write("\"D3@Sketch1\"=\"slot_length\"\n")
 
 
 textFile.close()
